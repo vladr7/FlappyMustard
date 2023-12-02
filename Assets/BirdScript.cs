662c84a52b968ce029c48b29945f8a45f5d4acad
Vlad <ricean.vlad7@gmail.com>
2023-12-02 16:46:39 +0200

    Add pipes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidbody;
+    public float flapStrength = 10;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * flapStrength;
+        }
     }
 }
