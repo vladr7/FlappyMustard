f0658db063028055f71a0d8439c82f3936ff3868
Vlad <ricean.vlad7@gmail.com>
2023-12-05 09:18:54 +0200

    Spawn random fruit

--- Assets/FruitSpawnerScript.cs
+++ Assets/FruitSpawnerScript.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class FruitSpawnerScript : MonoBehaviour
 {
-    public GameObject fruit;
-    public float horizontalLimit = 8f; 
+    public GameObject fruits;
+    public float horizontalLimit = 8f;
 
     // Start is called before the first frame update
     void Start()
     {
- 
     }
 
     // Update is called once per frame
@@ -25,13 +24,27 @@ public class FruitSpawnerScript : MonoBehaviour
         {
             SpawnFruit();
         }
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y));
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            Camera.main.transform.position.y));
         float clampedX = Mathf.Clamp(mousePosition.x, -horizontalLimit, horizontalLimit);
         transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 
+
+    public GameObject GetRandomFruit()
+    {
+        int childrenCount = fruits.transform.childCount;
+        if (childrenCount == 0) return null;
+
+        int randomIndex = Random.Range(0, childrenCount);
+        return fruits.transform.GetChild(randomIndex).gameObject;
+    }
+
+
     private void SpawnFruit()
     {
+        GameObject fruit = GetRandomFruit();
         Instantiate(fruit, transform.position, transform.rotation);
     }
 }
