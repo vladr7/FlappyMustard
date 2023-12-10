25f4611239d877f0021edd17830727fad1973410
Vlad <ricean.vlad7@gmail.com>
2023-12-10 09:38:29 +0200

    Improve visibility

--- Assets/LeaderboardScript.cs
+++ Assets/LeaderboardScript.cs
@@ -9,7 +9,7 @@ public class LeaderboardScript : MonoBehaviour
     public TextMeshProUGUI secondPlaceText;
     public TextMeshProUGUI thirdPlaceText;
     public TextMeshProUGUI fourthPlaceText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +19,20 @@ public class LeaderboardScript : MonoBehaviour
     void Update()
     {
     }
-    
+
     public void UpdateLeaderboard(List<PlayerNameScore> leaderboard)
     {
-        if(leaderboard.Count < 1 || leaderboard[0] == null)
+        if (leaderboard.Count < 1 || leaderboard[0] == null)
             return;
-        firstPlaceText.text = leaderboard[0].Name + leaderboard[0].Score;
-        if(leaderboard.Count < 2 || leaderboard[1] == null)
+        firstPlaceText.text = leaderboard[0].Name + " " + leaderboard[0].Score;
+        if (leaderboard.Count < 2 || leaderboard[1] == null)
             return;
-        secondPlaceText.text = leaderboard[1].Name + leaderboard[1].Score;
-        if(leaderboard.Count < 3 || leaderboard[2] == null)
+        secondPlaceText.text = leaderboard[1].Name + " " + leaderboard[1].Score;
+        if (leaderboard.Count < 3 || leaderboard[2] == null)
             return;
-        thirdPlaceText.text = leaderboard[2].Name + leaderboard[2].Score;
-        if(leaderboard.Count < 4 || leaderboard[3] == null)
+        thirdPlaceText.text = leaderboard[2].Name + " " + leaderboard[2].Score;
+        if (leaderboard.Count < 4 || leaderboard[3] == null)
             return;
-        fourthPlaceText.text = leaderboard[3].Name + leaderboard[3].Score;
+        fourthPlaceText.text = leaderboard[3].Name + " " + leaderboard[3].Score;
     }
 }
